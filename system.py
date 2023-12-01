995c64aadc58d585f5104edd01f428b549563f29
IsaiahLevy <il1016@nyu.edu>
2023-12-01 17:02:08 -0500

    added basic scoring system

--- system.py
+++ system.py
@@ -32,64 +32,84 @@ uri = "mongodb+srv://patent_data:patent_data@cluster0.fhrejvf.mongodb.net/?retry
 stemmer = PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
-# Check for JSON file argument
-if len(sys.argv) > 1:
-    json_file = sys.argv[1]
-    patents_data = fetch_data_from_json(json_file)
-else:
-    # Initialize MongoDB client
-    client = MongoClient(uri, server_api=ServerApi('1'))
+def main():
+    # Check for JSON file argument
+    if len(sys.argv) > 1:
+        json_file = sys.argv[1]
+        patents_data = fetch_data_from_json(json_file)
+    else:
+        # Initialize MongoDB client
+        client = MongoClient(uri, server_api=ServerApi('1'))
 
-    # Connect to the patent database and collection
-    db = client['patent_database']
-    collection = db['patents']
+        # Connect to the patent database and collection
+        db = client['patent_database']
+        collection = db['patents']
 
-    # Fetch patent data from MongoDB
-    patents_data = list(collection.find({}, {'_id': 0, 'processed_data': 1, 'patent_name': 1}))
+        # Fetch patent data from MongoDB
+        patents_data = list(collection.find({}, {'_id': 0, 'processed_data': 1, 'patent_name': 1}))
 
-    # Close the MongoDB connection
-    client.close()
+        # Close the MongoDB connection
+        client.close()
 
-# Extract the processed patent texts
-patent_texts = [patent['processed_data'] for patent in patents_data]
+    # Extract the processed patent texts
+    patent_texts = [patent['processed_data'] for patent in patents_data]
 
-# Preprocess the patent texts
-preprocessed_patents = [preprocess_text(text, stemmer, stop_words) for text in patent_texts]
+    # Preprocess the patent texts
+    preprocessed_patents = [preprocess_text(text, stemmer, stop_words) for text in patent_texts]
 
-# Create a dictionary representation of the documents
-dictionary = corpora.Dictionary(preprocessed_patents)
+    # Create a dictionary representation of the documents
+    dictionary = corpora.Dictionary(preprocessed_patents)
 
-# Create a bag-of-words corpus
-corpus = [dictionary.doc2bow(text) for text in preprocessed_patents]
+    # Create a bag-of-words corpus
+    corpus = [dictionary.doc2bow(text) for text in preprocessed_patents]
 
-# Create a TF-IDF model from the corpus
-tfidf_model = models.TfidfModel(corpus)
-corpus_tfidf = tfidf_model[corpus]
+    # Create a TF-IDF model from the corpus
+    tfidf_model = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf_model[corpus]
 
-# Load query terms
-with open('patent_queries.txt', 'r') as file:
-    queries = file.readlines()
-queries = [query.strip() for query in queries]
+    # Load query terms
+    with open('patent_queries.txt', 'r') as file:
+        queries = file.readlines()
+    queries = [query.strip() for query in queries]
 
-# Number of top patents to retrieve for each query
-top_n = 5
+    # Number of top patents to retrieve for each query
+    top_n = 5
 
-# Convert queries to TF-IDF space and find similarity
-for query in queries:
-    preprocessed_query = preprocess_text(query, stemmer, stop_words)
-    query_bow = dictionary.doc2bow(preprocessed_query)
-    query_tfidf = tfidf_model[query_bow]
+    # Store query results in a structured format
+    query_results = []
 
-    # Compute similarity for the entire corpus
-    similarities_index = similarities.MatrixSimilarity(tfidf_model[corpus], num_features=len(dictionary))
-    cosine_similarities = np.array(similarities_index[query_tfidf])
+    # Convert queries to TF-IDF space and find similarity
+    for query in queries:
+        preprocessed_query = preprocess_text(query, stemmer, stop_words)
+        query_bow = dictionary.doc2bow(preprocessed_query)
+        query_tfidf = tfidf_model[query_bow]
 
-    relevant_patents_indices = cosine_similarities.argsort()[-top_n:][::-1]  # Indices of top N relevant patents
+        # Compute similarity for the entire corpus
+        similarities_index = similarities.MatrixSimilarity(tfidf_model[corpus], num_features=len(dictionary))
+        cosine_similarities = np.array(similarities_index[query_tfidf])
 
-    print(f"\nMost relevant patents for query: '{query}'")
-    for index in relevant_patents_indices:
-        print(f"Patent: {patents_data[index]['patent_name']} (Score: {cosine_similarities[index]:.4f})")
+        relevant_patents_indices = cosine_similarities.argsort()[-top_n:][::-1]  # Indices of top N relevant patents
 
+        # Store relevant patents for this query
+        relevant_patents = []
+        for index in relevant_patents_indices:
+            relevant_patents.append({
+                "patent_name": patents_data[index]['patent_name'],
+                "score": float(cosine_similarities[index])
+            })
 
-# Close the MongoDB connection
-client.close()
+        # Add query and its relevant patents to results
+        query_results.append({
+            "query": query,
+            "relevant_patents": relevant_patents
+        })
+
+    # Save query results to a JSON file
+    with open('query_results.json', 'w') as file:
+        json.dump(query_results, file, indent=4)
+
+    print("query_results.json has been created.")
+
+# Call the main function
+if __name__ == "__main__":
+    main()
